0589be01a88cb4fac6f78e73ba478f6ce0356b3b
Nguyễn Quốc Khánh <nqk25092002@gmail.com>
2024-04-16 16:42:55 +0700

    khanh2909

--- User.php
+++ User.php
@@ -6,31 +6,38 @@ use App\Models\User;
 use App\Models\Category;
 use App\Models\Permission;
 
-class UserService {
-    public function __construct(User $user,
-        Category $category, Permission $permission) {
-        $this->user = $user;
-        $this->category = $category;
-        $this->permission = $permission;
-    }
+class UserService 
+{
+    public function __construct(
+        User $user,
+        Category $category, 
+        Permission $permission) 
+        {
+            $this->user = $user;
+            $this->category = $category;
+            $this->permission = $permission;
+        }
 
-    public function create_user($param)
+    public function createUser($param)
     {
         $user = [
             'name' => $param['name'],
-            'email' => $param['email']
+            'email' => $param['email'],
         ];
-        if(isset($param['password'] && $param['password'])){
-        	$user['password'] = $param['password']
+        if(isset($param['password'] && $param['password'])) {
+        	$user['password'] = $param['password'];
         }
-        return $this->user->create($user);
+         return $this->user->create($user);
     }
 
-    public function search($param){
-    	$UserSearch = $this->user;
+    public function search($param)
+    {
+    	$userSearch = $this->user;
     	if ($param['keyword']) {
-    		$UserSearch->where('name', "like","%$param['keyword']");
+    		$UserSearch->where('name', "like', "%{$param['keyword']}%");
     	}
-    	return $UserSearch->with("role",'group')->where('status', true)->whereHas('schedule')->paginate();
+    	 return $userSearch->with('role', 'group')->where('status', true)
+         ->whereHas('schedule')
+         ->paginate();
     }
 }
